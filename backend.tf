18fdeb983262ad99389764197d96a68721002e43
karthik-koantek <106896843+karthik-koantek@users.noreply.github.com>
2022-06-07 00:13:05 -0400

    Create backend.tf

---
+++ backend.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "s3" {
+    bucket = "terraform-state-buscket"
+    key    = "databricks/dev/terraform.tfstate"
+    region = "us-east-2"
+    dynamodb_table = "justanother"
+  }
+}
