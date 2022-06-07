bfefa71f129b1f585b6291d104cbdfe2b456c0e4
karthik-koantek <106896843+karthik-koantek@users.noreply.github.com>
2022-06-07 00:10:11 -0400

    Create provider.tf

---
+++ provider.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    databricks = {
+      source  = "databrickslabs/databricks"
+      version = "0.4.7"
+    }
+    aws = {
+      source  = "hashicorp/aws"
+      version = "3.49.0"
+    }
+  }
+}
+provider "aws" {
+  region = var.region
+  access_key = "${var.access_key}"
+  secret_key = "${var.secret_key}"
+}
+// initialize provider in "MWS" mode to provision new workspace
+provider "databricks" {
+  alias    = "mws"
+  host     = "https://accounts.cloud.databricks.com"
+  username = var.databricks_account_username
+  password = var.databricks_account_password
+  
+}
