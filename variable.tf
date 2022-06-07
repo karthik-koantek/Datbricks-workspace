343d0bdb42c987af527d14324f5d3ef0023b7055
karthik-koantek <106896843+karthik-koantek@users.noreply.github.com>
2022-06-07 00:10:43 -0400

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,17 @@
+variable "databricks_account_username" {}
+variable "databricks_account_password" {}
+variable "databricks_account_id" {}
+variable "access_key" {}
+variable "secret_key" {}
+variable "tags" {
+  default = {}
+}
+variable "cidr_block" {
+  default = "10.4.0.0/16"
+}
+variable "region" {
+  default = "us-east-2"
+}
+variable "name" {
+  default = "demokoantek"
+}
